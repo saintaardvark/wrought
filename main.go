69b1ff9e013496af25c5c7c5490a064e3c5bc639
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-05-05 13:37:09 -0700

    Refactor to use Ham struct
    Is a Ham struct a sandwich? (philosoraptor)
    
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- main.go
+++ main.go
@@ -16,57 +16,73 @@ import (
 // List of countries, states, cities
 // List of phrases
 
+// Ham is a ham
+type Ham struct {
+	Callsign string
+	Location string
+	Name     string
+}
+
 var (
 	callsignFile = "/home/aardvark/.qrq/toplist"
+	me           = Ham{
+		Callsign: "VA7UNX",
+		Location: "NEW WESTMINSTER BC CANADA",
+		Name:     "HUGH",
+	}
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	callsigns := readCallsigns()
-	cx := getRandomCallsign(callsigns)
-	cxLocation := "NEW WESTMINSTER BC CANADA"
-	cxName := "HUGH"
-	rx := getRandomCallsign(callsigns)
-	rxLocation := "DALLAS TX USA"
-	rxName := "JANE"
+	cx := &me
+	rx := newHam()
 	fmt.Printf(initialGreeting(cx, rx))
 	// printMorse(initialGreeting(cx, rx))
-	fmt.Println(firstExchange(rx, cx, cxName, cxLocation))
-	fmt.Println(secondExchange(rx, cx, rxName, rxLocation))
-	fmt.Println(gnightBob(cx, rx, rxName))
+	fmt.Println(firstExchange(rx, cx))
+	fmt.Println(secondExchange(rx, cx))
+	fmt.Println(gnightBob(cx, rx))
 }
 
-func initialGreeting(cx, rx string) string {
-	cxRepeat := fmt.Sprintf("%s %s %s", cx, cx, cx)
-	rxRepeat := fmt.Sprintf("%s %s %s", rx, rx, rx)
-	msg := fmt.Sprintf("CQ CQ CQ DE %s K\n%s DE %s KN\n", cxRepeat, cx, rxRepeat)
+func newHam() *Ham {
+	ham := Ham{
+		Callsign: getRandomCallsign(),
+		Location: "DALLAS TX USA",
+		Name:     "JANE",
+	}
+	return &ham
+}
+
+func initialGreeting(caller, receiver *Ham) string {
+	callerRepeat := fmt.Sprintf("%s %s %s", caller.Callsign, caller.Callsign, caller.Callsign)
+	receiverRepeat := fmt.Sprintf("%s %s %s", receiver.Callsign, receiver.Callsign, receiver.Callsign)
+	msg := fmt.Sprintf("CQ CQ CQ DE %s K\n%s DE %s KN\n", callerRepeat, caller.Callsign, receiverRepeat)
 	return msg
 }
 
-func firstExchange(rx, cx, cxName, cxLocation string) string {
-	msg := de(rx, cx) + " "
+func firstExchange(caller, receiver *Ham) string {
+	msg := de(receiver.Callsign, caller.Callsign) + " "
 	msg = msg + "TNX FOR CALL BT UR RST 599 599 HR "
-	msg = msg + qth(cxLocation) + " "
-	msg = msg + name(cxName) + " "
+	msg = msg + qth(caller.Location) + " "
+	msg = msg + name(caller.Name) + " "
 	msg = msg + "HW CPY? "
-	msg = msg + kn(rx, cx)
+	msg = msg + kn(receiver.Callsign, caller.Callsign)
 	return msg
 }
 
-func secondExchange(rx, cx, rxName, rxLocation string) string {
-	msg := de(rx, cx) + " "
+func secondExchange(caller, receiver *Ham) string {
+	msg := de(receiver.Callsign, caller.Callsign) + " "
 	msg = msg + "TNX FOR RPT SLD CPY FB UR RST 599 599 BT" + " "
-	msg = msg + name(rxName) + " "
-	msg = msg + qth(rxLocation) + " "
-	msg = msg + kn(rx, cx)
+	msg = msg + name(receiver.Name) + " "
+	msg = msg + qth(receiver.Location) + " "
+	msg = msg + kn(receiver.Name, caller.Name)
 	return msg
 }
 
-func gnightBob(cx, rx, rxName string) string {
-	msg := de(cx, rx) + " "
-	msg = msg + "TNX FER FB QSO " + rxName + " "
+func gnightBob(caller, receiver *Ham) string {
+	msg := de(caller.Callsign, receiver.Callsign) + " "
+	msg = msg + "TNX FER FB QSO " + receiver.Name + " "
 	msg = msg + "HP CU AGN BT VY 73 TO U ES URS SK" + " "
-	msg = msg + de(cx, rx)
+	msg = msg + de(caller.Callsign, receiver.Callsign)
 	return msg
 }
 
@@ -107,6 +123,7 @@ func readCallsigns() *[]string {
 	return &callsigns
 }
 
-func getRandomCallsign(cs *[]string) string {
+func getRandomCallsign() string {
+	cs := readCallsigns()
 	return (*cs)[rand.Intn(len(*cs))]
 }
