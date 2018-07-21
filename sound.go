bd4c70269782e1cde121ee3ace3023de4abcb01a
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-07-21 11:15:33 -0700

    Break this out to separate file
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

---
+++ sound.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"math"
+
+	"github.com/dbatbold/beep"
+)
+
+func doABeep(duration int) {
+	freqHertz := 500.0
+	vol := 80
+
+	freq := beep.HertzToFreq(freqHertz)
+	// Just send empty string
+	music := beep.NewMusic("")
+	playBeep(music, vol, duration, 1, freq)
+}
+
+func dit() {
+	doABeep(150)
+}
+
+func dah() {
+	doABeep(300)
+}
+
+func buildABeep(music *beep.Music, volume, duration, count int, freq float64) *[]int16 {
+	bar := beep.SampleAmp16bit * (float64(volume) / 100.0)
+	samples := int(beep.SampleRate64 * (float64(duration) / 1000.0))
+	rest := 0
+	if count > 1 {
+		rest = (beep.SampleRate / 20) * 4 // 200ms
+	}
+	buf := make([]int16, samples+rest)
+	var last int16
+	var fade = 1024
+	if samples < fade {
+		fade = 1
+	}
+	for i := range buf {
+		if i < samples-fade {
+			buf[i] = int16(bar * math.Sin(float64(i)*freq))
+			last = buf[i]
+		} else {
+			if last > 0 {
+				last -= 31
+			} else {
+				last += 31
+			}
+			buf[i] = last
+		}
+	}
+	return &buf
+}
+
+func justPlayBeep(music *beep.Music, buf *[]int16) {
+	beep.InitSoundDevice()
+	go music.Playback(*buf, *buf)
+	music.WaitLine()
+	beep.FlushSoundBuffer()
+}
+
+// Taken from github.com/dbatbold/beep; 2-term BSD license
+// Thanks, dbatbold!
+func playBeep(music *beep.Music, volume, duration, count int, freq float64) {
+	bar := beep.SampleAmp16bit * (float64(volume) / 100.0)
+	samples := int(beep.SampleRate64 * (float64(duration) / 1000.0))
+	rest := 0
+	if count > 1 {
+		rest = (beep.SampleRate / 20) * 4 // 200ms
+	}
+	buf := make([]int16, samples+rest)
+	var last int16
+	var fade = 1024
+	if samples < fade {
+		fade = 1
+	}
+	for i := range buf {
+		if i < samples-fade {
+			buf[i] = int16(bar * math.Sin(float64(i)*freq))
+			last = buf[i]
+		} else {
+			if last > 0 {
+				last -= 31
+			} else {
+				last += 31
+			}
+			buf[i] = last
+		}
+	}
+	beep.InitSoundDevice()
+	for i := 0; i < count; i++ {
+		go music.Playback(buf, buf)
+		music.WaitLine()
+	}
+	beep.FlushSoundBuffer()
+}
