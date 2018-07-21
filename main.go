a866cd3257640cb33074ada1bb4aaa3d41966239
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-07-21 10:39:47 -0700

    Add helper functions that should let me build a whole exchange
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- main.go
+++ main.go
@@ -205,6 +205,42 @@ func dah() {
 	doABeep(300)
 }
 
+func buildABeep(music *beep.Music, volume, duration, count int, freq float64) *[]int16 {
+	bar := beep.SampleAmp16bit * (float64(volume) / 100.0)
+	samples := int(beep.SampleRate64 * (float64(duration) / 1000.0))
+	rest := 0
+	if count > 1 {
+		rest = (beep.SampleRate / 20) * 4 // 200ms
+	}
+	buf := make([]int16, samples+rest)
+	var last int16
+	var fade = 1024
+	if samples < fade {
+		fade = 1
+	}
+	for i := range buf {
+		if i < samples-fade {
+			buf[i] = int16(bar * math.Sin(float64(i)*freq))
+			last = buf[i]
+		} else {
+			if last > 0 {
+				last -= 31
+			} else {
+				last += 31
+			}
+			buf[i] = last
+		}
+	}
+	return &buf
+}
+
+func justPlayBeep(music *beep.Music, buf *[]int16) {
+	beep.InitSoundDevice()
+	go music.Playback(*buf, *buf)
+	music.WaitLine()
+	beep.FlushSoundBuffer()
+}
+
 // Taken from github.com/dbatbold/beep; 2-term BSD license
 // Thanks, dbatbold!
 func playBeep(music *beep.Music, volume, duration, count int, freq float64) {
