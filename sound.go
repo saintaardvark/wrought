e2e162c4b93396859fc30a95eb7646396f025410
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-07-24 09:45:10 -0400

    Add buildABeep/playBeep functions
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- sound.go
+++ sound.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/dbatbold/beep"
@@ -22,42 +23,6 @@ func dit() {
 
 func dah() {
 	doABeep(300)
-}
-
-func buildABeep(music *beep.Music, volume, duration, count int, freq float64) *[]int16 {
-	bar := beep.SampleAmp16bit * (float64(volume) / 100.0)
-	samples := int(beep.SampleRate64 * (float64(duration) / 1000.0))
-	rest := 0
-	if count > 1 {
-		rest = (beep.SampleRate / 20) * 4 // 200ms
-	}
-	buf := make([]int16, samples+rest)
-	var last int16
-	var fade = 1024
-	if samples < fade {
-		fade = 1
-	}
-	for i := range buf {
-		if i < samples-fade {
-			buf[i] = int16(bar * math.Sin(float64(i)*freq))
-			last = buf[i]
-		} else {
-			if last > 0 {
-				last -= 31
-			} else {
-				last += 31
-			}
-			buf[i] = last
-		}
-	}
-	return &buf
-}
-
-func justPlayBeep(music *beep.Music, buf *[]int16) {
-	beep.InitSoundDevice()
-	go music.Playback(*buf, *buf)
-	music.WaitLine()
-	beep.FlushSoundBuffer()
 }
 
 // Taken from github.com/dbatbold/beep; 2-term BSD license
@@ -88,10 +53,53 @@ func playBeep(music *beep.Music, volume, duration, count int, freq float64) {
 			buf[i] = last
 		}
 	}
+	// fmt.Printf("[FIXME] buf: %+v\n", buf)
 	beep.InitSoundDevice()
 	for i := 0; i < count; i++ {
 		go music.Playback(buf, buf)
 		music.WaitLine()
 	}
+	beep.FlushSoundBuffer()
+}
+
+func buildABeep(music *beep.Music, volume, duration, count int, freq float64) *[]int16 {
+	fmt.Printf("[FIXME] buildABeep: vol: %d\n", volume)
+	fmt.Printf("[FIXME] buildABeep: duration: %d\n", duration)
+	fmt.Printf("[FIXME] buildABeep: freq: %f\n", freq)
+	bar := beep.SampleAmp16bit * (float64(volume) / 100.0)
+	samples := int(beep.SampleRate64 * (float64(duration) / 1000.0))
+	rest := 0
+	if count > 1 {
+		rest = (beep.SampleRate / 20) * 4 // 200ms
+	}
+	buf := make([]int16, samples+rest)
+	var last int16
+	var fade = 1024
+	if samples < fade {
+		fade = 1
+	}
+	for i := range buf {
+		if i < samples-fade {
+			buf[i] = int16(bar * math.Sin(float64(i)*freq))
+			last = buf[i]
+		} else {
+			if last > 0 {
+				last -= 31
+			} else {
+				last += 31
+			}
+			buf[i] = last
+		}
+	}
+	// fmt.Printf("[FIXME] buildABeep: buf: %+v\n", buf)
+	return &buf
+}
+
+func justPlayBeep(music *beep.Music, buf *[]int16) {
+	fmt.Println("[FIXME] Made it to justPlayBeep")
+	// fmt.Printf("[FIXME] *buf: %+v\n", *buf)
+	beep.InitSoundDevice()
+	go music.Playback(*buf, *buf)
+	music.WaitLine()
 	beep.FlushSoundBuffer()
 }
