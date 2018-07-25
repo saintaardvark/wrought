855ac0434ed2f9928554f8a71a7849352b6eff97
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-07-25 09:14:43 -0400

    Break out these methods to separate file
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- main.go
+++ main.go
@@ -66,61 +66,6 @@ func playMorse(s string) {
 	}
 }
 
-func initialGreeting(caller, receiver *ham.Ham) string {
-	callerRepeat := fmt.Sprintf("%s %s %s", caller.Callsign, caller.Callsign, caller.Callsign)
-	receiverRepeat := fmt.Sprintf("%s %s %s", receiver.Callsign, receiver.Callsign, receiver.Callsign)
-	msg := fmt.Sprintf("CQ CQ CQ DE %s K\n%s DE %s KN\n", callerRepeat, caller.Callsign, receiverRepeat)
-	return msg
-}
-
-func firstExchange(caller, receiver *ham.Ham) string {
-	msg := de(receiver.Callsign, caller.Callsign) + " "
-	msg = msg + "TNX FOR CALL BT UR RST 599 599 HR "
-	msg = msg + qth(caller.Location) + " "
-	msg = msg + name(caller.Name) + " "
-	msg = msg + "HW CPY? "
-	msg = msg + kn(caller.Callsign, receiver.Callsign)
-	return msg
-}
-
-func secondExchange(caller, receiver *ham.Ham) string {
-	msg := de(caller.Callsign, receiver.Callsign) + " "
-	msg = msg + "TNX FOR RPT SLD CPY FB UR RST 599 599 BT" + " "
-	msg = msg + name(receiver.Name) + " "
-	msg = msg + qth(receiver.Location) + " "
-	msg = msg + kn(caller.Callsign, receiver.Callsign)
-	return msg
-}
-
-func gnightBob(caller, receiver *ham.Ham) string {
-	msg := de(receiver.Callsign, caller.Callsign) + " "
-	msg = msg + "TNX FER FB QSO " + receiver.Name + " "
-	msg = msg + "HP CU AGN BT VY 73 TO U ES URS SK" + " "
-	msg = msg + de(receiver.Callsign, caller.Callsign) + "\n"
-	// And now the reply
-	msg = msg + de(caller.Callsign, receiver.Callsign) + " "
-	msg = msg + "TNX FER QSO " + caller.Name + " "
-	msg = msg + "BCNU BT VY 73 TO U ES URS SK" + " "
-	msg = msg + de(caller.Callsign, receiver.Callsign)
-	return msg
-}
-
-func name(name string) string {
-	return fmt.Sprintf("NAME %s %s", name, name)
-}
-
-func qth(location string) string {
-	return fmt.Sprintf("QTH %s %s", location, location)
-}
-
-func de(cx, rx string) string {
-	return fmt.Sprintf("%s DE %s", cx, rx)
-}
-
-func kn(cx, rx string) string {
-	return fmt.Sprintf("%s KN", de(cx, rx))
-}
-
 func printMorse(msg string) {
 	fmt.Println(morse.EncodeITU(msg))
 }
